fc4b8dee2c9774842cb640a63bd2cba79baa51b9
Adrien Charruel <adrien.charruel@tagheuer.com>
2022-07-27 17:33:32 +0200

    Support mouse5 and mouse6 for workspace control

--- src/pointer.rs
+++ src/pointer.rs
@@ -1,37 +1,48 @@
 use input::event::pointer::Axis::*;
+use input::event::pointer::ButtonState::*;
+use input::event::pointer::PointerButtonEvent;
 use input::event::{pointer::PointerScrollWheelEvent, PointerEvent};
 use log::debug;
 
 use crate::commands::SwaypedCommand;
 
-enum SwaypedScrollDir {
-    Left,
-    Right,
+const MOUSE5: u32 = 275;
+const MOUSE6: u32 = 276;
+
+enum SwaypedPointerAction {
+    ScrollLeft,
+    ScrollRight,
+    Button5,
+    Button6,
 }
 
-impl SwaypedScrollDir {
+impl SwaypedPointerAction {
     fn process_command(self) {
         let cmd: SwaypedCommand = match self {
-            SwaypedScrollDir::Left => SwaypedCommand::WorkspacePrev,
-            SwaypedScrollDir::Right => SwaypedCommand::WorkspaceNext,
+            SwaypedPointerAction::ScrollLeft => SwaypedCommand::WorkspacePrev,
+            SwaypedPointerAction::ScrollRight => SwaypedCommand::WorkspaceNext,
+            SwaypedPointerAction::Button5 => SwaypedCommand::WorkspaceBackAndForth,
+            SwaypedPointerAction::Button6 => SwaypedCommand::WorkspaceNew,
         };
         cmd.process_command()
     }
 
     fn display(&self) -> String {
         match self {
-            SwaypedScrollDir::Left => "LEFT".to_string(),
-            SwaypedScrollDir::Right => "RIGHT".to_string(),
+            SwaypedPointerAction::ScrollLeft => "SCROLL LEFT".to_string(),
+            SwaypedPointerAction::ScrollRight => "SCROLL RIGHT".to_string(),
+            SwaypedPointerAction::Button5 => "MOUSE5".to_string(),
+            SwaypedPointerAction::Button6 => "MOUSE6".to_string(),
         }
     }
 }
 
 fn pointer_handle_scroll_event(event: PointerScrollWheelEvent) {
     let horiz = event.scroll_value_v120(Horizontal);
-    let scroll: Option<SwaypedScrollDir> = if horiz > 0.0 {
-        Some(SwaypedScrollDir::Right)
+    let scroll: Option<SwaypedPointerAction> = if horiz > 0.0 {
+        Some(SwaypedPointerAction::ScrollRight)
     } else if horiz < 0.0 {
-        Some(SwaypedScrollDir::Left)
+        Some(SwaypedPointerAction::ScrollLeft)
     } else {
         None
     };
@@ -42,10 +53,30 @@ fn pointer_handle_scroll_event(event: PointerScrollWheelEvent) {
     }
 }
 
+fn pointer_handle_button(event: PointerButtonEvent) {
+    if event.button_state() != Released {
+        return;
+    }
+
+    let button: Option<SwaypedPointerAction> = if event.button() == MOUSE5 {
+        Some(SwaypedPointerAction::Button5)
+    } else if event.button() == MOUSE6 {
+        Some(SwaypedPointerAction::Button6)
+    } else {
+        None
+    };
+
+    if let Some(b) = button {
+        debug!("button press! {:?}", b.display());
+        b.process_command();
+    }
+}
+
 pub fn pointer_handle_event(event: PointerEvent) {
     // only interested in scroll wheel event
     match event {
         PointerEvent::ScrollWheel(e) => pointer_handle_scroll_event(e),
+        PointerEvent::Button(e) => pointer_handle_button(e),
         _ => (),
     }
 }
